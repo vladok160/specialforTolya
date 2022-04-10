fb17b8e78f9d0ddd054df45ec2e0305bc7baa1f3
Влад Зеленин <zeleninvlad16022001@gmail.com>
2022-04-10 16:29:08 +0400

    Добавил новый метод для дешифровки

---
+++ WindowsFormsApp1/Deshifr.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Deshifr
+    {
+        public static string DeshifrText(string roma)
+        {
+            public string rashifr(char[] text_for_decrypt, char[] arr_decrypt_letters)
+            {
+                char[] arr_encrypt_letters = { ' ', 'о', 'а', 'е', 'и', 'т', 'н', 'л', 'р', 'с', 'в', 'к', 'м', 'д', 'у', 'п', 'б', 'г', 'ы', 'ч', 'ь', 'з', 'я', 'й', 'х', 'ж', 'ш', 'ю', 'ф', 'э', 'щ', 'ё', 'ц', 'ъ' };
+                char[] arr_encrypt_text = { };
+                Dictionary<char, char> zip = new Dictionary<char, char>();
+            }
+            return "ss";
+        }
+    }
+}
