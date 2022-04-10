9615403dee1292161d836464b213c1476de184dc
Влад Зеленин <zeleninvlad16022001@gmail.com>
2022-04-10 16:49:18 +0400

    Более менее рабочий вариант

--- WindowsFormsApp1/Deshifr.cs
+++ WindowsFormsApp1/Deshifr.cs
@@ -8,14 +8,13 @@ namespace WindowsFormsApp1
 {
     class Deshifr
     {
-        public static string DeshifrText(string roma)
+        public static string DeshifrText(string string_for_decrypt, string arr_decrypt_letters)
         {
-            public string rashifr(char[] text_for_decrypt, char[] arr_decrypt_letters)
-            {
-                char[] arr_encrypt_letters = { ' ', 'о', 'а', 'е', 'и', 'т', 'н', 'л', 'р', 'с', 'в', 'к', 'м', 'д', 'у', 'п', 'б', 'г', 'ы', 'ч', 'ь', 'з', 'я', 'й', 'х', 'ж', 'ш', 'ю', 'ф', 'э', 'щ', 'ё', 'ц', 'ъ' };
-                char[] arr_encrypt_text = { };
-                Dictionary<char, char> zip = new Dictionary<char, char>();
-            }
+            char[] text_for_decrypt = string_for_decrypt.ToCharArray();
+            //Символы по убыванию встречаемости в русском языке
+            char[] arr_encrypt_letters = { ' ', 'о', 'а', 'е', 'и', 'т', 'н', 'л', 'р', 'с', 'в', 'к', 'м', 'д', 'у', 'п', 'б', 'г', 'ы', 'ч', 'ь', 'з', 'я', 'й', 'х', 'ж', 'ш', 'ю', 'ф', 'э', 'щ', 'ё', 'ц', 'ъ' };
+            char[] arr_encrypt_text = { };
+            Dictionary<char, char> zip = new Dictionary<char, char>();
             return "ss";
         }
     }
