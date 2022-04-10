704e5fe08fc3f7036ddfd2d19411b9b0315cc681
Anatoly <mikhalevanatolii@gmail.com>
2022-04-10 15:50:00 +0300

    ыыы раманаф

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -44,6 +44,7 @@ namespace WindowsFormsApp1
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(159, 191);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
